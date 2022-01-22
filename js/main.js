da84e17089f2137339fc842b8dbc72f7ad345bfb
Лиза Бандура <lizabandura@MacBook-Air-Liza.local>
2022-01-22 13:24:48 +0200

    start project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const dropdown = new Dropdown('#dropdown', {
+  items: [
+    { label: 'Москва', id: 'spb' },
+    { label: 'Киев', id: 'kiev' },
+    { label: 'Одесса', id: 'od' },
+    { label: 'Николаев', id: 'nik' },
+    { label: 'Питер', id: 'pt' },
+    { label: 'Ney York', id: 'new' }
+  ]
+});
